15d6753a16f0adcf65a5b6f5a891d1ba3c6778db
d.gorban <dg@itm.tech>
2023-12-06 19:13:50 +0100

    move mainHandler to separate precode.go

--- precode_test.go
+++ precode_test.go
@@ -4,51 +4,12 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-var cafeList = map[string][]string{
-	"moscow": {"Мир кофе", "Сладкоежка", "Кофе и завтраки", "Сытый студент"},
-}
-
-func mainHandle(w http.ResponseWriter, req *http.Request) {
-	countStr := req.URL.Query().Get("count")
-	if countStr == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("count missing"))
-		return
-	}
-
-	count, err := strconv.Atoi(countStr)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong count value"))
-		return
-	}
-
-	city := req.URL.Query().Get("city")
-
-	cafe, ok := cafeList[city]
-	if !ok {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong city value"))
-		return
-	}
-
-	if count > len(cafe) {
-		count = len(cafe)
-	}
-
-	answer := strings.Join(cafe[:count], ",")
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(answer))
-}
 
 func urlWithQuery(city string, count int) string {
 	return fmt.Sprintf("/cafe?city=%s&count=%d", city, count)
@@ -62,7 +23,7 @@ func TestMainHandlerWithValidParams(t *testing.T) {
 	require.NoError(t, err)
 
 	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
+	handler := http.HandlerFunc(MainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
 	assert.Equal(t, http.StatusOK, responseRecorder.Code, "handler returned wrong status code, expected 200")
@@ -77,7 +38,7 @@ func TestMainHandlerWithInvalidCityName(t *testing.T) {
 	require.NoError(t, err)
 
 	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
+	handler := http.HandlerFunc(MainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
 	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code, "handler returned wrong status code, expected 400")
@@ -95,13 +56,13 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	require.NoError(t, err)
 
 	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
+	handler := http.HandlerFunc(MainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
 	responseBody := responseRecorder.Body.String()
 	response := strings.Split(responseBody, ",")
 
-	expectedCafesLen := len(cafeList["moscow"])
+	expectedCafesLen := len(CafeList["moscow"])
 
 	assert.Len(t, response, expectedCafesLen, "unexpected number of cafes in response")
 }
